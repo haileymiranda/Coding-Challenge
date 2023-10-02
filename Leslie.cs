a04fe3ec3f40db15398abf65e2d5defc1a51fc26
haileymiranda <145374157+haileymiranda@users.noreply.github.com>
2023-10-02 12:14:52 -0400

    Add files via upload

---
+++ Leslie.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leslie : MonoBehaviour
+{
+
+
+    string Variable1;
+    string Variable2;
+    string Variable3;
+    string Variable4;
+    string mySentence;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Variable1 = "I";
+        Variable2 = "want";
+        Variable3 = "100";
+        Variable4 = "Pancakes";
+
+        mySentence = "I want 100 Pancakes.";
+        
+        Debug.Log(mySentence);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      
+
+    }
+}
